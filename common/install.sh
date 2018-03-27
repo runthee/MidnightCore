210df30aab7e11fda4d058a47caf9f08e597f962
OldMidnight <fareedidris20@gmail.com>
2018-03-28 00:39:43 +0100

    added volume button options, integrated seamless beta stream updates

--- common/install.sh
+++ common/install.sh
@@ -5,13 +5,99 @@ if [ ! -d /system/xbin ]; then
 else
   bin=xbin
 fi
-# stable version backup for beta users
-if [ -f /sdcard/MidBack/midnight ]
-then
-  ui_print "- Beta tester detected!"
-  ui_print "- Setting backup files..."
-  rm -f /sdcard/MidBack/midnight
-	cp -f $INSTALLER/system/$bin/midnight /sdcard/MidBack
+#Beta test Choice
+BETA=false
+STABLE=false
+# Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
+chmod 755 $INSTALLER/common/keycheck
+
+keytest() {
+  ui_print "- Vol Key Test -"
+  ui_print "   Press Vol Up:"
+  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
+  return 0
+}
+
+chooseport() {
+  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
+  while (true); do
+    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
+    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+      break
+    fi
+  done
+  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+    return 0
+  else
+    return 1
+  fi
+}
+
+chooseportold() {
+  # Calling it first time detects previous input. Calling it second time will do what we want
+  $INSTALLER/common/keycheck
+  $INSTALLER/common/keycheck
+  SEL=$?
+  if [ "$1" == "UP" ]; then
+    UP=$SEL
+  elif [ "$1" == "DOWN" ]; then
+    DOWN=$SEL
+  elif [ $SEL -eq $UP ]; then
+    return 0
+  elif [ $SEL -eq $DOWN ]; then
+    return 1
+  else
+    ui_print "   Vol key not detected!"
+    abort "   Use name change method in TWRP"
+  fi
+}
+
+if ! $BETA && ! $STABLE; then
+  if keytest; then
+    FUNCTION=chooseport
+  else
+    FUNCTION=chooseportold
+    ui_print "   ! Legacy device detected! Using old keycheck method"
+    ui_print " "
+    ui_print "- Vol Key Programming -"
+    ui_print "   Press Vol Up Again:"
+    $FUNCTION "UP"
+    ui_print "   Press Vol Down"
+    $FUNCTION "DOWN"
+  fi
+  ui_print " "
+  ui_print "- Select Option -"
+  ui_print "   Install into beta stream: "
+  ui_print "   Vol+ = Become beta tester, Vol- = Remain on stable."
+  ui_print " "
+  if $FUNCTION; then
+    BETA=true
+  else
+    STABLE=true
+  fi
+fi
+if $STABLE; then
+  ui_print "- Remaining on stable stream..."
+  # stable version backup for previous beta users
+  if [ -f /sdcard/MidBack/midnight ]; then
+  ui_print "- Previous beta tester detected!"
+    ui_print "- Setting backup files..."
+    rm -f /sdcard/MidBack/midnight
+  	cp -f $INSTALLER/system/$bin/midnight /sdcard/MidBack
+  fi
+else
+  ui_print "- Backing up stable stream..."
+	if [ ! -d /sdcard/MidBack ]; then
+    mkdir /sdcard/MidBack
+  else
+    rm -f /sdcard/MidBack/midnight
+  fi
+  mv -f $INSTALLER/system/$bin/midnight /sdcard/MidBack
+  ui_print "- Switching to beta stream..."
+  LINK="https://ncloud.zaclys.com/index.php/s/75I9STT9yhqGZyJ/download"
+  wget -q -O $INSTALLER/custom/midnight "$LINK"
+  mv -f $INSTALLER/custom/midnight $INSTALLER/system
+  echo "Done!"
 fi
 # Font survival
 if [ -e /sdcard/MidnightMain/MidnightFonts/tmp.txt ]
--- common/uninstall.sh
+++ common/uninstall.sh
@@ -1,1 +1,85 @@
+DELETE=false
+DONTDEL=false
+# Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
+chmod 755 $INSTALLER/common/keycheck
 
+keytest() {
+  ui_print "- Vol Key Test -"
+  ui_print "   Press Vol Up:"
+  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
+  return 0
+}
+
+chooseport() {
+  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
+  while (true); do
+    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
+    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+      break
+    fi
+  done
+  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+    return 0
+  else
+    return 1
+  fi
+}
+
+chooseportold() {
+  # Calling it first time detects previous input. Calling it second time will do what we want
+  $INSTALLER/common/keycheck
+  $INSTALLER/common/keycheck
+  SEL=$?
+  if [ "$1" == "UP" ]; then
+    UP=$SEL
+  elif [ "$1" == "DOWN" ]; then
+    DOWN=$SEL
+  elif [ $SEL -eq $UP ]; then
+    return 0
+  elif [ $SEL -eq $DOWN ]; then
+    return 1
+  else
+    ui_print "   Vol key not detected!"
+    abort "   Use name change method in TWRP"
+  fi
+}
+
+if ! $DELETE && ! $DONTDEL; then
+  if keytest; then
+    FUNCTION=chooseport
+  else
+    FUNCTION=chooseportold
+    ui_print "   ! Legacy device detected! Using old keycheck method"
+    ui_print " "
+    ui_print "- Vol Key Programming -"
+    ui_print "   Press Vol Up Again:"
+    $FUNCTION "UP"
+    ui_print "   Press Vol Down"
+    $FUNCTION "DOWN"
+  fi
+  ui_print " "
+  ui_print "- Select Option -"
+  ui_print "   Remove all files related to MidnightCore: "
+  ui_print "   Vol+ = Remove all files, Vol- = Do not remove files."
+  ui_print "   Removing files will delete all backups as well."
+  if $FUNCTION; then
+    DELETE=true
+  else
+    DONTDEL=true
+  fi
+fi
+if $DONTDEL; then
+  ui_print "- No files removed."
+else
+  ui_print "- Removing related files..."
+  if [ -d /sdcard/MidnightMain ]; then
+    rm -r /sdcard/MidnightMain
+  fi
+  if [ -d /sdcard/'MidnightMain(Beta)' ]; then
+    rm -r /sdcard/'MidnightMain(Beta)'
+  fi
+  if [ -d /sdcard/MidBack ]; then
+    rm -r /sdcard/MidBack
+  fi
+fi
+	
