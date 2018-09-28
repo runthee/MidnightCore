b8202345ad8c55e2ef9063a0a0d74be990cb9824
Fareed Idris <fareed.idris@protonmail.com>
2018-09-28 12:36:00 +0100

    Updated to Unity v1.7.1, Cleaner UI(*), Removed Misc and DNS Modules, Only Includes Fonts Module, Dev commit for testing purposes.

--- common/install.sh
+++ common/install.sh
@@ -1,149 +1,108 @@
-# xbin detection
+# XBIN DETECT
 if [ ! -d /system/xbin ]; then
-  mv -f $INSTALLER/system/xbin $INSTALLER/system/bin
-  bin=bin
+    mv $INSTALLER/system/xbin $INSTALLER/system/bin
+    bin=bin
 else
-  bin=xbin
+    bin=xbin
 fi
 
-if grep -q 'beta' $UNITY$SYS/$bin/midnight
-then
-  mm='MidnightMain(Beta)'
-else
-  mm='MidnightMain'
-fi
-
-# GET OLD/NEW FROM ZIP NAME
-case $(basename $ZIP) in
-  *beta*|*Beta*|*BETA*) BETA=true;;
-  *stable*|*STABLE*|*STABLE*) STABLE=true;;
-esac
-
-# Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
 chmod 755 $INSTALLER/common/keycheck
 
 keytest() {
-  ui_print " - Vol Key Test -"
-  ui_print "   Press Vol Up:"
-  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
-  return 0
+    ui_print " [#] Vol key Test-"
+    ui_print " [+] Press Vol Up:"
+    (/system/bin/getevent -lc 1 2>$1 | /system/bin/grep VOLUME | /system/bin/grep "DOWN" > $INSTALLER/events) || return 1
+    return 0
 }
 
 chooseport() {
-  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
-  while (true); do
-    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
-    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
-      break
+    while (true); do
+        /system/bin/getevent -lc 1 2>$1 | /system/bin/grep VOLUME | /system/bin/grep "DOWN" > $INSTALLER/events
+        if ( `cat $INSTALLR/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null` ); then
+            break
+        fi
+    done
+    if ( `cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null` ); then
+        return 0
+    else
+        return 1
     fi
-  done
-  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
-    return 0
-  else
-    return 1
-  fi
 }
 
 chooseportold() {
-  # Calling it first time detects previous input. Calling it second time will do what we want
-  $KEYCHECK
-  $KEYCHECK
-  SEL=$?
-  if [ "$1" == "UP" ]; then
-    UP=$SEL
-  elif [ "$1" == "DOWN" ]; then
-    DOWN=$SEL
-  elif [ $SEL -eq $UP ]; then
-    return 0
-  elif [ $SEL -eq $DOWN ]; then
-    return 1
-  else
-    ui_print "   Vol key not detected!"
-    abort "   Use name change method in TWRP"
-  fi
+    $KEYCHECK
+    $KEYCHECK
+    SEL=$?
+    if [ "$1" == "UP" ]; then
+        UP=$SEL
+    elif [ "$1" == "DOWN" ]; then
+        DOWN=$SEL
+    elif [ $SEL -eq $UP ]; then
+        return 0
+    elif [ $SEL -eq $DOWN ]; then
+        return 1
+    else
+        ui_print " [!] Vol key not detected!"
+        abort " [!] Send this install log to the module creator!"
+    fi
 }
 
-if [ -z $BETA ]; then
-  if keytest; then
+ui_print ""
+if keytest; then
     FUNCTION=chooseport
-  else
+else
     FUNCTION=chooseportold
-    ui_print "   ! Legacy device detected! Using old keycheck method"
-    ui_print " "
-    ui_print "- Vol Key Programming -"
-    ui_print "   Press Vol Up Again:"
+    ui_print " [!] Legacy device detected! Using old keycheck method"
+    ui_print ""
+    ui_print " [#] Vol Key Programming"
+    ui_print " [+] Press Vol Up Again:"
     $FUNCTION "UP"
-    ui_print "   Press Vol Down"
+    ui_print " [+] Press Vol Down:"
     $FUNCTION "DOWN"
-  fi
-  ui_print " "
-  ui_print "- Select Option -"
-  ui_print "   Install into beta stream: "
-  ui_print "   Vol+ = Become beta tester, Vol- = Remain on stable."
-  ui_print " "
-  if $FUNCTION; then
-    BETA=true
-  else
-    STABLE=true
-  fi
-else
-  ui_print "- Option specified in Zipname!"
 fi
-if $STABLE; then
-  ui_print "- Remaining on stable stream..."
-  # stable version backup for previous beta users
-  if [ -f /sdcard/MidBack/midnight ]; then
-    ui_print "- Previous beta tester detected!"
-    ui_print "- Setting backup files..."
-    rm -f /sdcard/MidBack/midnight
-    cp -f $INSTALLER/system/$bin/midnight /sdcard/MidBack
-  fi
-else
-  ui_print "- Backing up stable stream..."
-	if [ ! -d /sdcard/MidBack ]; then
-    mkdir /sdcard/MidBack
-  else
-    rm -f /sdcard/MidBack/midnight
-  fi
-  mv -f $INSTALLER/system/$bin/midnight /sdcard/MidBack
-  ui_print "- Switching to beta stream..."
-  mv -f $INSTALLER/custom/midnight $INSTALLER/system/$bin
-  echo "Done!"
-fi
-# Font survival
-if [ -e /sdcard/$mm/MidnightFonts/tmp.txt ]
-then
-  FONT="$( cat /sdcard/$mm/MidnightFonts/tmp.txt | head -n 1 | tr -d ' ' )"
-  FONT2="$( echo $FONT | cut -d ')' -f 2 )"
-	ui_print "- Applying: $FONT2"
-  if [ -e /sdcard/MidnightMain/MidnightFonts/Backup/$FONT2.tar ]
-  then
-    ui_print "- Restoring applied font..."
-    cd /sdcard/$mm/MidnightFonts/Backup
-    tar -xf $FONT2.tar sbin/.core/img/MidnightCore/system/fonts/
-    cd /
-    if [ ! -d /sbin/.core/img/MidnightCore/system/fonts ]
-    then
-      mkdir $INSTALLER/system/fonts
+# FONT SURVIVAL
+if [ -f /data/media/0/MidnightMain/MidnightFonts/currently_applied_font.txt ]; then
+    FONTTEXT="$( cat /data/media/0/MidnightMain/MidnightFonts/currently_applied_font.txt | tr -d ' ' )"
+    FONT="$( echo $FONTTEXT | cut -d ']' -f 2 )"
+    ui_print " [+] Currently Applied: $FONT"
+    ui_print " [+] Would you like to restore this font?"
+    ui_print " [!] IF YOU DONT HAVE A BACKUP, YOU NEED AN INTERNET CONNECTION!"
+    ui_print " [+] Vol+ = Yes, Vol- = No"
+    if $FUNCTION; then
+        if [ -f /data/media/0/MidnightMain/MidnightFonts/Backup/$FONT.tar.gz ]; then
+            ui_print " [+] Font Backup Found!"
+            ui_print " [+] Restoring Backup..."
+            dir=pwd
+            mkdir $INSTALLER/system/fonts
+            cd /data/media/0/MidnightMain/MidnightFonts/Backup
+            tar -xzf "$FONT.tar.gz" -C $INSTALLER/system/fonts
+            cd $dir
+            ui_print " [+] Backup Restored!"
+        else
+            ui_print " [+] No Font Backup Found."
+            ui_print " [+] Setting Up Font Restoration Environment..."
+            ui_print " [+] Checking Internet Connection..."
+            if $BOOTMODE; then
+                wget -q -O /data/media/0/DONT-DELETE-2 "https://ncloud.zaclys.com/index.php/s/HQpbpeNKYp5crlz/download"
+                ui_print " [+] Links List downloaded..."
+                FONTNUM="$( cat /data/media/0/MidnightMain/MidnightFonts/currently_applied_font.txt | cut -d ']' -f 1 | cut -d '[' -f 2 )"
+                LINK="$( cat /data/media/0/DONT-DELETE-2 | xargs | cut -d ' ' -f $FONTNUM )"
+                ui_print " [+] Downloading Font..."
+                wget -q -O /data/media/0/$FONT.zip $LINK
+                ui_print " [+] $FONT Downloaded!"
+                mkdir /data/media/0/tmpmidfontdir > /dev/null 2>&1
+                unzip -o /data/media/0/$FONT.zip -d /data/media/0/tmpmidfontdir > /dev/null 2>&1
+                mv -f /data/media/0/tmpmidfontdir/system/* $INSTALLER/system > /dev/null 2>&1
+                rm -f /data/media/0/$FONT.zip > /dev/null 2>&1
+                rm -rf /data/media/0/tmpmidfontdir > /dev/null 2>&1
+                rm -f /data/media/0/DONT-DELETE-2
+                ui_print " [+] Font Restored!"
+            else
+                ui_print " [!] TWRP Installation Detected!"
+                ui_print " [+] Skipping Font Restoration."
+            fi
+        fi
+    else
+        ui_print " [+] Resuming Process..."
     fi
-    cp -rf /sdcard/$mm/MidnightFonts/Backup/sbin/.core/img/MidnightCore/system/fonts "$INSTALLER"/system>&2
-    rm -rf /sdcard/$mm/MidnightFonts/Backup/sbin
-    ui_print "- Font restored!"
-  else
-    ui_print "- Setting up font restoration environment"
-    wget -q -O /sdcard/DONT-DELETE-2 "https://ncloud.zaclys.com/index.php/s/HQpbpeNKYp5crlz/download"
-    ui_print "- Restoring applied font..."
-    FONTNUM="$( cat /sdcard/$mm/MidnightFonts/tmp.txt | head -n 1 | cut -d ')' -f 1 )"
-    LINK="$( cat /sdcard/DONT-DELETE-2 | xargs | cut -d " " -f $FONTNUM )"
-    ui_print "- Downloading font..."
-    wget -q -O /sdcard/$FONT2.zip $LINK
-    mkdir /sdcard/tmpfont
-    unzip -o /sdcard/$FONT2.zip -d /sdcard/tmpfont>&2
-    cp -rf /sdcard/tmpfont/system/* $INSTALLER/system>&2
-    ui_print "- Font restored!"
-  fi
 fi
-# Cleanup
-rm -f /sdcard/DONT-DELETE-2 2>/dev/null
-rm -f /sdcard/$FONT2.zip 2>/dev/null
-rm -rf /sdcard/tmpfont 2>/dev/null
--- common/uninstall.sh
+++ common/uninstall.sh
@@ -1,87 +1,78 @@
 case $(basename $ZIP) in
-  *delete*|*Delete*|*DELETE*) DELETE=true;;
+    *delete*|*Delete*|*DELETE*)
+        DELETE=true
+        ;;
 esac
 
-# Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
 chmod 755 $INSTALLER/common/keycheck
 
 keytest() {
-  ui_print " - Vol Key Test -"
-  ui_print "   Press Vol Up:"
-  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
-  return 0
+    ui_print " [#] Volume Key Test"
+    ui_print " [+] Press Vol Up:"
+    (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
+    return 0
 }
 
 chooseport() {
-  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
-  while (true); do
-    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
-    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
-      break
+    while (true); do
+        /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
+        if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+            break
+        fi
+    done
+    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+        return 0
+    else
+        return 1
     fi
-  done
-  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
-    return 0
-  else
-    return 1
-  fi
 }
 
 chooseportold() {
-  # Calling it first time detects previous input. Calling it second time will do what we want
-  $KEYCHECK
-  $KEYCHECK
-  SEL=$?
-  if [ "$1" == "UP" ]; then
-    UP=$SEL
-  elif [ "$1" == "DOWN" ]; then
-    DOWN=$SEL
-  elif [ $SEL -eq $UP ]; then
-    return 0
-  elif [ $SEL -eq $DOWN ]; then
-    return 1
-  else
-    ui_print "   Vol key not detected!"
-    abort "   Use name change method in TWRP"
-  fi
+    $KEYCHECK
+    $KEYCHECK
+    SEL=$?
+    if [ "$1" == "UP" ]; then
+        UP=$SEL
+    elif [ "$1" == "DOWN" ]; then
+        DOWN=$SEL
+    elif [ $SEL -eq $UP ]; then
+        return 0
+    elif [ $SEL -eq $DOWN ]; then
+        return 1
+    else
+        ui_print " [!] Volume key not detected!"
+        abort " [!] Use name change method in TWRP!"
+    fi
 }
 
 if [ -z $DELETE ]; then
-  if keytest; then
-    FUNCTION=chooseport
-  else
-    FUNCTION=chooseportold
-    ui_print "   ! Legacy device detected! Using old keycheck method"
+    if keytest; then
+        FUNCTION=chooseport
+    else
+        FUNCTION=chooseportold
+        ui_print " [!] Legacy device detected! Using old keycheck method"
+        ui_print " "
+        ui_print " [#] Volume Key Programming"
+        ui_print " [+] Press Vol Up Again:"
+        $FUNCTION "UP"
+        ui_print " [+] Press Vol Down:"
+        $FUNCTION "DOWN"
+    fi
     ui_print " "
-    ui_print "- Vol Key Programming -"
-    ui_print "   Press Vol Up Again:"
-    $FUNCTION "UP"
-    ui_print "   Press Vol Down"
-    $FUNCTION "DOWN"
-  fi
-  ui_print " "
-  ui_print "- Select Option -"
-  ui_print "   Remove all files related to MidnightCore: "
-  ui_print "   Vol+ = Remove all files, Vol- = Do not remove files."
-  ui_print "   Removing files will delete all backups as well."
-  if $FUNCTION; then
-    DELETE=true
-  else
-    KEEP=true
-  fi
+    ui_print " [#] Module Cleanup"
+    ui_print " [+] Remove ALL files related to MidnightCore?"
+    ui_print " [+] This will also remove any backups of fonts or media files."
+    ui_print " [+] Vol+ = Remove all files, Vol- = Don't remove files"
+    if $FUNCTION; then
+        DELETE=true
+    else
+        DELETE=false
+    fi
 fi
-if $KEEP; then
-  ui_print "- No files removed."
+if $DELETE; then
+    ui_print " [+] Purging MidnightCore Files..."
+    rm -rf /data/media/0/MidnightMain > /dev/null 2>&1
+    ui_print " [+] All MidnightCore Files Purged!"
 else
-  ui_print "- Removing related files..."
-  if [ -d /sdcard/MidnightMain ]; then
-    rm -rf /sdcard/MidnightMain
-  fi
-  if [ -d /sdcard/'MidnightMain(Beta)' ]; then
-    rm -rf /sdcard/'MidnightMain(Beta)'
-  fi
-  if [ -d /sdcard/MidBack ]; then
-    rm -rf /sdcard/MidBack
-  fi
+    ui_print " [+] No Files Deleted"
 fi
-	
